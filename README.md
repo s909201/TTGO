ab036a4f7b77e8ba10da86c8b4f28864a25d8c7e
Morgan <morganchuang@elytone.com.tw>
2022-06-21 08:39:10 +0800

    add TTGO_Tutorial01

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Examples codes based on TTGO
 | IDE                | Arduino IDE   | 1.8.16  |
 
 # Revision History
+- 20220620 : add TTGO_Tutorial01, example code from https://www.youtube.com/watch?v=WFVjsxFMbSM&ab_channel=VolosProjects
+
 - 20220620 : add TTGOAlienAttack, example code from https://www.youtube.com/watch?v=b8254--ibmM&ab_channel=VolosProjects
 
 - 20220620 : add ttgo-2, 傑森創工 example code from https://blog.jmaker.com.tw/ttgo-t-display/
---
+++ TTGO_Tutorial01/TTGO_Tutorial01.ino
@@ -0,0 +1,31 @@
+// TTGO Tutorial
+// Reference: https://www.youtube.com/watch?v=WFVjsxFMbSM&ab_channel=VolosProjects
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+void setup() {
+  // put your setup code here, to run once:
+  tft.init();
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_RED);
+  tft.drawString("Hello World!",10,10);
+  tft.setTextColor(TFT_GREEN);
+  tft.drawString("This is Font 2",10,30,2);
+  tft.setTextColor(TFT_BLUE);
+  tft.drawString("Font 4",10,50,4);
+  
+  tft.setTextColor(TFT_YELLOW, TFT_BLUE);
+  tft.drawString("5",30,100,5);
+  
+}
+
+int number = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tft.drawString(String(number), 30, 100, 7);
+  number++;
+  delay(500);
+}
